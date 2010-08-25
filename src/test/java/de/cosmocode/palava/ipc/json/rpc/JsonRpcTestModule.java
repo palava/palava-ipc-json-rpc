7daa5ec4e36ba4d28b3096a3c2fa5fdd34146f00
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-08-25 19:21:39 +0000

    fixed checkstyle issue

--- src/test/java/de/cosmocode/palava/ipc/json/rpc/JsonRpcTestModule.java
+++ src/test/java/de/cosmocode/palava/ipc/json/rpc/JsonRpcTestModule.java
@@ -26,7 +26,7 @@ import de.cosmocode.palava.core.inject.TypeConverterModule;
 import de.cosmocode.palava.core.lifecycle.LifecycleModule;
 import de.cosmocode.palava.ipc.DefaultIpcCallFilterChainFactoryModule;
 import de.cosmocode.palava.ipc.IpcEventModule;
-import de.cosmocode.palava.ipc.IpcModule;
+import de.cosmocode.palava.ipc.IpcScopeModule;
 import de.cosmocode.palava.ipc.execvm.LocalExecutorModule;
 import de.cosmocode.palava.ipc.session.store.IpcSessionStore;
 import de.cosmocode.palava.ipc.session.store.StoreIpcSessionModule;
@@ -50,7 +50,7 @@ public final class JsonRpcTestModule implements Module {
         binder.install(new BackgroundSchedulerModule());
         binder.install(new DefaultThreadProviderModule());
         binder.install(new FakeMBeanServerModule());
-        binder.install(new IpcModule());
+        binder.install(new IpcScopeModule());
         binder.install(new IpcEventModule());
         binder.install(new LocalExecutorModule());
         binder.install(new DefaultIpcCallFilterChainFactoryModule());
